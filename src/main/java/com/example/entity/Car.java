a8b253f4149fdae3d5750851ba8b42bbafdbfc6d
orange <chanpanhavuth.chea@gmail.com>
2023-03-30 19:31:26 +0700

    Increase Tables

--- src/main/java/com/example/entity/Car.java
+++ src/main/java/com/example/entity/Car.java
@@ -28,6 +28,10 @@ public class Car extends BaseEntity {
     private CarModel model_name;
 
     @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category_name;
+
+    @ManyToOne
     @JoinColumn(name = "branch_name_id")
     private Branch branch_name;
 
